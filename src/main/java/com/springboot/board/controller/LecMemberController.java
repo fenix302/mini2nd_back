a64ac475a770b14017c3303c022c9c59f21c9149
fenix302 <fenix302@naver.com>
2023-03-02 17:14:10 +0900

    수강신청기능

--- src/main/java/com/springboot/board/controller/LecMemberController.java
+++ src/main/java/com/springboot/board/controller/LecMemberController.java
@@ -1,13 +1,14 @@
 package com.springboot.board.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springboot.board.model.LecMember;
 import com.springboot.board.service.LecMemberService;
 
-
+@CrossOrigin
 @RestController
 public class LecMemberController {
 	@Autowired
